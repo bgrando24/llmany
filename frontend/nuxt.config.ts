95da2c9b7b76f7e9f58a6acb74ca95bbba5226ab
bgrando24 <bgrando24@gmail.com>
2025-05-25 21:28:19 +1000

    rebuilt frontend project

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/icon']
-})
+    compatibilityDate: "2025-05-15",
+    devtools: { enabled: true },
+    modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/icon", "@nuxtjs/tailwindcss"],
+    css: ["~/assets/css/main.css"],
+});
---
+++ frontend/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1 class="text-green-500">Hello world!</h1>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
